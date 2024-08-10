45c0bfc3ec6ea01157adb9712e3a55867e666c1c
Zeeshan Khan <zeeshan.khan124@hotmail.com>
2024-08-09 22:52:29 -0700

    updated

--- my-app1/src/heroes/page.jsx
+++ my-app1/src/heroes/page.jsx
@@ -21,9 +21,7 @@ export const Hero1 = () => (
           </p>
         </div>
         <div className="flex flex-row gap-3">
-          <Button size="lg" className="gap-4" variant="outline">
-            Jump on a call <PhoneCall className="w-4 h-4" />
-          </Button>
+         
           <Button size="lg" className="gap-4">
             Sign up here <MoveRight className="w-4 h-4" />
           </Button>
